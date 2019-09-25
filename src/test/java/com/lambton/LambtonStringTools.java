b988f90b5d074c74d5d244d5f445136c46359216
macstudent <kaurkamaljit963@gmail.com>
2019-09-25 19:10:50 -0400

    reverse()

--- src/test/java/com/lambton/LambtonStringTools.java
+++ src/test/java/com/lambton/LambtonStringTools.java
@@ -1,21 +1,51 @@
 package com.lambton;
 
 
+import sun.awt.image.IntegerComponentRaster;
+
 public class LambtonStringTools
 {
-    String reverseString(String Str)
-    {
 
-        String string1=Str;
-        String reverse=" ";
-        for (int i = (string1.length()) - 1; i >= 0; i--)
-        {
-            reverse= reverse +string1.charAt(i);
+    String reverseString(String Str) {
 
-        }
-        return reverse;
+                                     String string1 = Str;
+                                     String reverse = " ";
+                                    for (int i = (string1.length()) - 1; i >= 0; i--) {
+                                                  reverse = reverse + string1.charAt(i);
 
+                                             }
+                                                return reverse;
 
-    }
+                    }
+
+    int binaryTODecimal(String num) {
+        String binaryString = num;
+        int decimal = Integer.parseInt(binaryString, 2);
+        return decimal;
+                  }
+
+                  String initials(String name)
+                  {
+                      String S1=name.substring(0);
+                      char a=name.charAt(0);
+                      char b;
+
+                      String S2=name.substring(S1.length()+1);
+                      
+                      String S3;
+                      return  S1;
+
+                  }
 
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/test/java/com/lambton/MainClass.java
+++ src/test/java/com/lambton/MainClass.java
@@ -1,11 +1,16 @@
 package com.lambton;
 
+import java.sql.SQLOutput;
+
 public class MainClass
 {
     public static void main(String[] args)
     {
         LambtonStringTools lst=new LambtonStringTools();
-       
+
+
+
         System.out.println(lst.reverseString("Lambton"));
+        System.out.println(lst.binaryTODecimal("1000"));
     }
 }
